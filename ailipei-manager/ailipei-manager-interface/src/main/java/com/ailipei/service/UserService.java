46164d6a5fba06d85a6aa62f0ead30ea1157730d
chenguangyong <1192165267@qq.com>
2019-02-15 21:54:47 +0800

    接口

--- ailipei-manager/ailipei-manager-interface/src/main/java/com/ailipei/service/UserService.java
+++ ailipei-manager/ailipei-manager-interface/src/main/java/com/ailipei/service/UserService.java
@@ -1,7 +1,24 @@
 package com.ailipei.service;
 
+
+import java.util.List;
+
+import com.ailipei.pojo.PagerResult;
 import com.ailipei.pojo.TbUser;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 
 public interface UserService {
+	/**
+	 * 根据用户id查询
+	 * @param userId
+	 * @return TbUser
+	 */
    TbUser getUserById(long userId);
+  /**
+   * 分页查询用户数据
+   * @param page
+   * @param rows
+   * @return
+   */
+   PagerResult getTbUserList(int page, int rows);
 }
